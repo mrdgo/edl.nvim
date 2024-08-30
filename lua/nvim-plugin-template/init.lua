01b7e16026da1bc78fdaf92d1b8afb88c9783fa6
Maxim Onciul <onciul@cs.fau.de>
2024-08-30 21:41:28 +0200

    dbg: add setup

--- lua/nvim-plugin-template/init.lua
+++ lua/nvim-plugin-template/init.lua
@@ -1,7 +1,11 @@
-local function example()
-  return true
-end
-
 return {
-  example = example,
+  setup = function()
+    local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+    parser_config.edl = {
+      install_info = {
+        url = 'https://github.com/Andrade/tree-sitter-edl',
+        files = { 'src/parser.c' },
+      },
+    }
+  end,
 }
