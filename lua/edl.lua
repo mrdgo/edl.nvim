64a66bab491b5c8a210a0df71031cc0efda87e24
Maxim Onciul <onciul@cs.fau.de>
2024-09-06 08:09:05 +0200

    refactor: simpler filetype detection

--- lua/edl.lua
+++ lua/edl.lua
@@ -1,18 +1,17 @@
 return {
-  setup = function()
-    local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-    parser_config.edl = {
-      install_info = {
-        url = 'https://github.com/Andrade/tree-sitter-edl',
-        files = { 'src/parser.c' },
-      },
-    }
+	setup = function()
+		local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+		parser_config.edl = {
+			install_info = {
+				url = "https://github.com/Andrade/tree-sitter-edl",
+				files = { "src/parser.c" },
+			},
+		}
 
-    local au_id = vim.api.nvim_create_augroup('detect_ft_edl', { clear = true })
-    vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-      pattern = '*.edl',
-      group = au_id,
-      command = 'set ft=edl',
-    })
-  end,
+		vim.filetype.add({
+			extension = {
+				edl = "edl",
+			},
+		})
+	end,
 }
